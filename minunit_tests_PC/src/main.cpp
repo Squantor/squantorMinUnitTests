52152f4595adfc76ce14b5c20b9e8d4dea19174e
Squantor <squantmuts@gmail.com>
2020-12-29 15:40:17 +0100

    mu check works

--- minunit_tests_PC/src/main.cpp
+++ minunit_tests_PC/src/main.cpp
@@ -1,32 +1,16 @@
 /*
-MIT License
-
-Copyright (c) 2019 Bart Bilos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2020-2020 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
 #include <MinUnit.h>
 #include <stdio.h>
 #include <iostream>
 
 MINUNIT_ADD(foo)
 {
+    minUnitCheck(1 == 1);
     printf("executing foo\n");
 }
 
@@ -38,5 +22,7 @@ MINUNIT_ADD(bar)
 //int main(int argc, char *argv[])
 int main()
 {
-    return MINUNIT_RUN();;
+    // manually test minUnitCheck and minUnitFail
+
+    return MINUNIT_RUN();
 }
