c60e8bf6f71fdca346e6fa9e3870c1dc41c5591b
Squantor <squantmuts@gmail.com>
2022-04-16 13:29:10 +0200

    reformatted files and added failure callback tests

--- minunit_tests_PC/src/main.cpp
+++ minunit_tests_PC/src/main.cpp
@@ -4,47 +4,48 @@
  * Copyright (c) 2020-2020 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-/** 
+/**
  * \file main.cpp
- * 
+ *
  * Main entrypoint for executing minunit tests
- * 
- */ 
+ *
+ */
 #include <MinUnit.h>
 #include <stdio.h>
 
+int failureCallbacks = 0;
+
 /** \brief overridden reporting function
- * 
+ *
  * Executes the minunit test framework and checks the results
  */
-void minunitReport(const char * message)
-{
-    printf("%s", message);
+void minunitReport(const char *message) {
+  printf("%s", message);
+}
+
+/** \brief overridden Failure callback
+ */
+void minunitFailCallback(void) {
+  failureCallbacks++;
 }
 
 /** \brief main entrypoint
- * 
+ *
  * Executes the minunit test framework and checks the results
  */
-int main()
-{
-    minunitRun();
-    // check test state
-    if(minunitTestState.executed != 5)
-    {
-        printf("incorrect number of tests executed\n");
-    }
-    else if(minunitTestState.checks != 13)
-    {
-        printf("incorrect number of checks executed\n");
-    }
-    else if(minunitTestState.failures != 0)
-    {
-        printf("Tests that have failed\n");
-    }
-    else
-    {
-        printf("minunit tests passed\n");
-    }
-    return 0;
+int main() {
+  minunitRun();
+  // check test state
+  if (minunitTestState.executed != 5) {
+    printf("incorrect number of tests executed\n");
+  } else if (minunitTestState.checks != 15) {
+    printf("incorrect number of checks executed\n");
+  } else if (minunitTestState.failures != 0) {
+    printf("Tests that have failed\n");
+  } else if (failureCallbacks != 2) {
+    printf("Incorrect number of failure callbacks \n");
+  } else {
+    printf("minunit tests passed\n");
+  }
+  return 0;
 }
--- minunit_tests_PC/src/minunitTests.cpp
+++ minunit_tests_PC/src/minunitTests.cpp
@@ -6,114 +6,105 @@
  */
 #include <MinUnit.h>
 
-/** 
+/**
  * \file minunitTests.cpp
- * 
+ *
  * Tests to execute minunit test functionality
- * 
- */ 
+ *
+ */
 
 int testSetupExecuted;
 
 /**
  * \brief test testSetup function
  */
-MINUNIT_SETUP(minunitTestSetup)
-{
-    minUnitPass();  // added to supress warnings
-    testSetupExecuted = 123456789;
-    return;
+MINUNIT_SETUP(minunitTestSetup) {
+  minUnitPass();  // added to supress warnings
+  testSetupExecuted = 123456789;
+  return;
 }
 
 /**
  * \brief test testTeardown function
  */
-MINUNIT_TEARDOWN(minunitTestTeardown)
-{
-    minUnitPass();  // added to supress warnings
-    return;
+MINUNIT_TEARDOWN(minunitTestTeardown) {
+  minUnitPass();  // added to supress warnings
+  return;
 }
 
 /**
  * \brief Test setup and teardown functions
  */
-MINUNIT_ADD(minunitTestSetupFunc, minunitTestSetup, minunitTestTeardown)
-{
-    minUnitCheck(testSetupExecuted == 123456789);
-    // I cant see how to test teardown just yet
+MINUNIT_ADD(minunitTestSetupFunc, minunitTestSetup, minunitTestTeardown) {
+  minUnitCheck(testSetupExecuted == 123456789);
+  // I cant see how to test teardown just yet
 }
 
 /**
  * \brief helper function to test automatic failure
  */
-void TestBodyMinUnitFail( minunitState *testResults)
-{
-    minUnitFail();
+void TestBodyMinUnitFail(minunitState *testResults) {
+  minUnitFail();
 }
 
 /**
  * \brief Test passing mutest check
  */
-MINUNIT_ADD(minunitTestmutestFail, NULL, NULL)
-{
-    minunitState localState;
-    localState.executed = 0;
-    localState.checks = 0;
-    localState.failures = 0;
-    localState.flagFailed = 0;
-    TestBodyMinUnitFail(&localState);
-    minUnitCheck(localState.executed == 0);
-    minUnitCheck(localState.checks == 1);
-    minUnitCheck(localState.failures == 0);
-    minUnitCheck(localState.flagFailed == 1);
+MINUNIT_ADD(minunitTestmutestFail, NULL, NULL) {
+  minunitState localState;
+  localState.executed = 0;
+  localState.checks = 0;
+  localState.failures = 0;
+  localState.flagFailed = 0;
+  TestBodyMinUnitFail(&localState);
+  minUnitCheck(localState.executed == 0);
+  minUnitCheck(localState.checks == 1);
+  minUnitCheck(localState.failures == 0);
+  minUnitCheck(localState.flagFailed == 1);
 }
 
 /**
  * \brief helper function to test passing mutest check
  */
-void TestBodyMinUnitCheckFail( minunitState *testResults)
-{
-    minUnitCheck(1 == 0);
+void TestBodyMinUnitCheckFail(minunitState *testResults) {
+  minUnitCheck(1 == 0);
 }
 
 /**
  * \brief Test passing mutest check
  */
-MINUNIT_ADD(minunitTestCheckFail, NULL, NULL)
-{
-    minunitState localState;
-    localState.executed = 0;
-    localState.checks = 0;
-    localState.failures = 0;
-    localState.flagFailed = 0;
-    TestBodyMinUnitCheckFail(&localState);
-    minUnitCheck(localState.executed == 0);
-    minUnitCheck(localState.checks == 1);
-    minUnitCheck(localState.failures == 0);
-    minUnitCheck(localState.flagFailed == 1);
+MINUNIT_ADD(minunitTestCheckFail, NULL, NULL) {
+  minunitState localState;
+  localState.executed = 0;
+  localState.checks = 0;
+  localState.failures = 0;
+  localState.flagFailed = 0;
+  TestBodyMinUnitCheckFail(&localState);
+  minUnitCheck(localState.executed == 0);
+  minUnitCheck(localState.checks == 1);
+  minUnitCheck(localState.failures == 0);
+  minUnitCheck(localState.flagFailed == 1);
 }
 
 /**
  * \brief helper function to test passing mutest check
  */
-void TestBodyMinUnitCheckPass( minunitState *testResults)
-{
-    minUnitCheck(1 == 1);
+void TestBodyMinUnitCheckPass(minunitState *testResults) {
+  minUnitCheck(1 == 1);
 }
 
 /**
  * \brief Test passing mutest check
  */
-MINUNIT_ADD(minunitTestCheckPass, NULL, NULL)
-{
-    minunitState localState;
-    localState.executed = 0;
-    localState.checks = 0;
-    localState.failures = 0;
-    localState.flagFailed = 0;
-    TestBodyMinUnitCheckPass(&localState);
-    minUnitCheck(localState.executed == 0);
-    minUnitCheck(localState.checks == 1);
-    minUnitCheck(localState.failures == 0);
-    minUnitCheck(localState.flagFailed == 0);
+MINUNIT_ADD(minunitTestCheckPass, NULL, NULL) {
+  minunitState localState;
+  localState.executed = 0;
+  localState.checks = 0;
+  localState.failures = 0;
+  localState.flagFailed = 0;
+  TestBodyMinUnitCheckPass(&localState);
+  minUnitCheck(localState.executed == 0);
+  minUnitCheck(localState.checks == 1);
+  minUnitCheck(localState.failures == 0);
+  minUnitCheck(localState.flagFailed == 0);
 }
